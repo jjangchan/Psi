9adefbb3de907ef92bc6aa21c2db4386ecfa94af
jjangchan <rudcks6806@gamil.com>
2021-04-30 17:58:41 +0900

    Upload Ex2

---
+++ Example/Ex1_Reference.h
@@ -0,0 +1,8 @@
+//
+// Created by momantic03 on 2021-04-30.
+//
+
+#ifndef PSI_EX1_REFERENCE_H
+#define PSI_EX1_REFERENCE_H
+
+#endif //PSI_EX1_REFERENCE_H
---
+++ Example/Ex2_New_Delete.h
@@ -0,0 +1,8 @@
+//
+// Created by momantic03 on 2021-04-30.
+//
+
+#ifndef PSI_EX2_NEW_DELETE_H
+#define PSI_EX2_NEW_DELETE_H
+
+#endif //PSI_EX2_NEW_DELETE_H
