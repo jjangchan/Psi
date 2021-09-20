c3dcfc2881386a917bdb9857720480b5dbddd65e
jjangchan <rudcks6806@gmail.com>
2021-09-20 17:36:58 +0900

    merge

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include "Example/Ex5MyString.h"
 
+template <typename T>
+void println(T *t, int data_size)
+{
+    for(int i = 0; i < data_size; i++) std::cout << t[i] << std::endl;
+}
 int main() {
+    double *d = new double[100];
+    for(int i = 0; i < 100; i++) d[i] = i;
+    println(d, 100);
+
+    delete d;
     return 0;
 }
