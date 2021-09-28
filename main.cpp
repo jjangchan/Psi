bae9b215ca346bc5d884e35b62d462476161e69a
jjangchan <rudcks6806@gamil.com>
2021-09-28 10:07:07 +0900

    mergin

--- main.cpp
+++ main.cpp
@@ -1,94 +1,28 @@
 #include <iostream>
 #include <cstring>
 
-class MyString {
-    char *string_content;  // 문자열 데이터를 가리키는 포인터
-    int string_length;     // 문자열 길이
-
-    int memory_capacity;  // 현재 할당된 용량
-
+class Test{
+private:
+    int a;
 public:
-    MyString();
-
-    // 문자열로 부터 생성
-    MyString(const char *str);
-
-    // 복사 생성자
-    MyString(const MyString &str);
-
-    void reserve(int size);
-    MyString operator+(const MyString &s);
-    ~MyString();
-
-    int length() const;
-
-    void print();
-    void println();
+    Test(int a) : a(a){}
+    Test(const Test& t): a(t.a){std::cout <<"copy : " << this << std::endl;}
+    ~Test(){}
+    Test& operator++() {
+        a++;
+        return *this;
+    }
+    Test operator++(int){
+        Test temp(*this);
+        a++;
+        return temp;
+    }
+    Test& operator=(const Test& t){
+        a = t.a;
+        return *this;
+    }
 };
 
-MyString::MyString() {
-    std::cout << "call constructor ! " << std::endl;
-    string_length = 0;
-    memory_capacity = 0;
-    string_content = nullptr;
-}
-
-MyString::MyString(const char *str) {
-    std::cout << "call constructor ! " << std::endl;
-    std::cout << this << std::endl;
-    string_length = strlen(str);
-    memory_capacity = string_length;
-    string_content = new char[string_length];
-
-    for (int i = 0; i != string_length; i++) string_content[i] = str[i];
-}
-MyString::MyString(const MyString &str) {
-    std::cout << "call copy constructor ! " << std::endl;
-    string_length = str.string_length;
-    memory_capacity = str.string_length;
-    string_content = new char[string_length];
-
-    for (int i = 0; i != string_length; i++)
-        string_content[i] = str.string_content[i];
-}
-MyString::~MyString() { delete[] string_content; }
-void MyString::reserve(int size) {
-    if (size > memory_capacity) {
-        char *prev_string_content = string_content;
-
-        string_content = new char[size];
-        memory_capacity = size;
-
-        for (int i = 0; i != string_length; i++)
-            string_content[i] = prev_string_content[i];
-
-        if (prev_string_content != nullptr) delete[] prev_string_content;
-    }
-}
-MyString MyString::operator+(const MyString &s) {
-    MyString str;
-    str.reserve(string_length + s.string_length);
-    for (int i = 0; i < string_length; i++)
-        str.string_content[i] = string_content[i];
-    for (int i = 0; i < s.string_length; i++)
-        str.string_content[string_length + i] = s.string_content[i];
-    str.string_length = string_length + s.string_length;
-    return str;
-}
-int MyString::length() const { return string_length; }
-void MyString::print() {
-    for (int i = 0; i != string_length; i++) std::cout << string_content[i];
-}
-void MyString::println() {
-    for (int i = 0; i != string_length; i++) std::cout << string_content[i];
-
-    std::cout << std::endl;
-}
-
 int main() {
-    MyString str1("abc");
-    MyString str2("def");
-    std::cout << "-------------" << std::endl;
-    MyString str3 = str1 + str2;
-    str3.println();
+    return 0;
 }
