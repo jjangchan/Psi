4e48e0a55d9fe4e08e33fea62813ecd67c2ece62
jjangchan <rudcks6806@gamil.com>
2021-10-14 18:00:26 +0900

    upload array

--- Example/ex7-array.h
+++ Example/ex7-array.h
@@ -7,26 +7,83 @@
 
 class Array{
 private:
-    Array *array_number;
-    int data;
-    int n;
-    int size;
-    int capacity;
-
+    const int dimension;
+    int *size;
+    struct Address{
+       int level;
+       void* next;
+    };
+    Address* top;
 
 public:
-    Array(int n ):n(n){
-        array_number = new Array(n);
+    Array(int dimension, int* array_size):dimension(dimension){
+        size = new int [dimension];
+        for(int i = 0; i < dimension; i++) size[i] = array_size[i];
+        top = new Address;
+        top->level = 0;
+        InitializeAddress(top);
     }
-    Array(const Array& array):n(array.n){}
+    Array(const Array& array) : dimension(array.dimension) {
+        size = new int[dimension];
+        for(int i = 9; i < dimension; i++) size[i] = array.size[i];
+
+        top = new Address;
+        top->level = 0;
+
+        InitializeAddress(top);
+        CopyAddress(top, array.top);
+    }
     ~Array(){
-        delete array_number;
+        DeleteAddress(top);
+        if(size != nullptr){
+            delete[] size;
+            size = nullptr;
+        }
     }
     Array& operator[](const int n1){
-        std::cout << "call : " << n1 << std::endl;
+        //std::cout << "call : " << n1 << std::endl;
         return *this;
     }
 private:
+    void InitializeAddress(Address* current){
+        if(current == nullptr) return;
+        if(current->level == dimension-1){
+            current->next = new int[size[current->level]];
+            return;
+        }
+        current->next = new Address[size[current->level]];
+        // BFS Search
+        for(int i = 0; i != size[current->level]; i++){
+            (static_cast<Address*>(current->next)+i)->level = current->level+1;
+            InitializeAddress(static_cast<Address*>(current->next)+i);
+        }
+    }
+
+    void DeleteAddress(Address* current){
+        if(current == nullptr) return;
+        for(int i = 0; current->level < dimension-1 && i < size[current->level]; i++){
+            DeleteAddress(static_cast<Address*>(current->next)+i);
+        }
+        if(current->level == dimension-1){
+            delete[] static_cast<int*>(current->next);
+        }
+        else{
+            delete[] static_cast<Address*>(current->next);
+        }
+    }
+
+    void CopyAddress(Address* dst, Address* src){
+        if(dst->level == dimension-1){
+            for(int i = 0; i < size[dst->level]; i++) static_cast<int*>(dst->next)[i] = static_cast<int*>(src->next)[i];
+            return;
+        }
+        for(int i = 0; i != size[dst->level]; i++){
+            Address *new_dst = static_cast<Address*>(dst->next)+i;
+            Address *new_src = static_cast<Address*>(src->next)+i;
+            CopyAddress(new_dst, new_src);
+        }
+    }
 };
+
 
 #endif //EX2_NEW_DELETE_H_EX7_ARRAY_H
--- main.cpp
+++ main.cpp
@@ -2,35 +2,10 @@
 #include <chrono>
 #include "Example/ex7-array.h"
 
-class Base{
-public:
-    std::string s1;
-public:
-    Base() : s1("base"){ std::cout << "base class" << std::endl;}
-    void println() const {std::cout << s1 << std::endl;}
-
-};
-
-class Derived : public Base{
-private:
-    std::string s;
-public:
-    Derived() : Base(), s("derived") {
-        std::cout << "derived class" << std::endl;
-    }
-    void println() const {std::cout << s << std::endl;}
-};
 
 int main() {
-    Base b;
-    Derived d;
-
-    Base* p_b = &d;
-    Derived* p_d= static_cast<Derived*>(p_b);
-
-    p_d->println();
-
-    //Array array;
-    //array[2][2][3]; // -> array.operator[](2),operator[](2).operator[](3);
+    int dimension = 3;
+    int size[3] = {3,2,2};
+    Array(dimension, size);
     return 1;
 }
